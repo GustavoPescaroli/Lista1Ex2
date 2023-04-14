4dffe863d009ae6d22117d3c023ab9b4494d6235
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-14 20:04:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double e;
+            double n1;
+            double R;
+            //aresta
+            Console.WriteLine("valor da aresta");
+            n1 = double.Parse(Console.ReadLine());
+            // e é igual a n1
+            e = n1;
+            //R é igual a e multiplicado po e
+            R = n1 * e;
+
+            //resultado
+
+            Console.WriteLine("valor da aresta {0}", R);
+        }
+    }
+}
